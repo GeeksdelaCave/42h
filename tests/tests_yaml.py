037becba56fd00d974ada394f154c1dd330b22f1
Tristan Dietz <tristan.dietz@epita.fr>
2019-03-04 22:58:02 +0100

    [ADD] Adding colors and ending stats

--- tests/tests_yaml.py
+++ tests/tests_yaml.py
@@ -4,7 +4,22 @@ import subprocess
 import yaml
 
 
+# Color dictionary
+color_dict = {
+    'RED': '31',
+    'GREEN': '32',
+    'YELLOW': '33',
+    'BLUE': '34',
+    'L_GRAY': '37',
+    'L_RED': '91',
+    'L_GREEN': '92',
+    'L_YELLOW': '93',
+    'L_BLUE': '94',
+    'WHITE': '97'
+}
 
+
+# Launch process in child process
 def run(cmd):
     return subprocess.run(cmd,
                           shell=True,
@@ -13,38 +28,62 @@ def run(cmd):
                           stderr=subprocess.PIPE)
 
 
+# Change output color
 def set_color(color):
-    return "\e[0m" if color == 'RESET' else "\033[" + color_dict[color] + "m"
+    return "\033[0m" if (color == 'RESET') else "\033[" + color_dict[color] + "m"
 
+def main():
+    with open("tests_yaml.yml", "r") as f:
+        tests = yaml.load(f)
 
-with open("tests_yaml.yml", "r") as f:
-    tests = yaml.load(f)
+    ok = 0
+    ko = 0
 
-for test, data in tests.items():
-    ref = run("bash --posix -c '" + data["ref"] + "'")
-    cmd = run("bash --posix -c '" + data["cmd"] + "'")
+    for test, data in tests.items():
+        ref = run("bash --posix -c '" + data["ref"] + "'")
+        cmd = run("bash --posix -c '" + data["cmd"] + "'")
 
-    #  Listing des erreurs qui sont arrivés
-    errors = []
-    if ref.stdout != cmd.stdout:
-        errors.append("STDOUT")
-    if ref.stderr != cmd.stderr:
-        errors.append("STDERR")
-    if ref.returncode != cmd.returncode:
-        errors.append("RETURN CODE")
+        errors = []
+        if ref.stdout != cmd.stdout:
+            errors.append("STDOUT")
+        if ref.stderr != cmd.stderr:
+            errors.append("STDERR")
+        if ref.returncode != cmd.returncode:
+            errors.append("RETURN CODE")
 
-    res = "ERROR" if errors else "OK"
-    print("{id}: {desc} [{res}]".format(id=test, desc=data["desc"], res=res))
+        # OK in green or ERROR in red
+        if (errors):
+            res = set_color('L_RED') + "[ERROR]"
+            ko += 1
+        else:
+            res = set_color('L_GREEN') + "[OK]"
+            ok += 1
 
-    # decode() convertit des bytes en str
-    if "STDOUT" in errors:
-        print("\t", "stdout returned: " + cmd.stdout, end='')
-        print("\t", "stdout expected: " + ref.stdout, end='')
+        # Output
+        log = f'  {set_color("L_YELLOW")}{test:13} {set_color("L_GRAY")}{data["desc"]:50} {res:12}{set_color("RESET")}'
+        print(log)
 
-    if "STDERR" in errors:
-        print("\t", "stderr returned: " + cmd.stderr, end='')
-        print("\t", "stderr expected: " + ref.stderr, end='')
+        if "STDOUT" in errors:
+            print(f'\t\t{set_color("RED")}stdout returned: {set_color("WHITE")}'
+                  + cmd.stdout, end='')
+            print(f'\t\t{set_color("GREEN")}stdout expected: {set_color("WHITE")}'
+                  + ref.stdout, end='')
 
-    if "RETURN CODE" in errors:
-        print("\t", "return code returned: " + cmd.returncode, end='')
-        print("\t", "return code expected: " + ref.returncode, end='')
+        if "STDERR" in errors:
+            print(f'\t\t{set_color("RED")}stderr returned: {set_color("WHITE")}'
+                  + cmd.stderr, end='')
+            print(f'\t\t{set_color("GREEN")}stderr expected: {set_color("WHITE")}'
+                  + ref.stderr, end='')
+
+        if "RETURN CODE" in errors:
+            print(f'\t\t{set_color("RED")}return code returned: {set_color("WHITE")}'
+                  + cmd.returncode, end='')
+            print(f'\t\t{set_color("GREEN")}return code expected: {set_color("WHITE")}'
+                  + ref.returncode, end='')
+
+    print(f'\n\nRan {set_color("YELLOW")}{ok + ko}{set_color("RESET")} tests.')
+    print(f'{ok}{set_color("GREEN")} passed{set_color("RESET")} ({(ok * 100) // (ko + ok)}.{ok % (ko + ok) * 100 // 100}%)')
+    print(f'{ko}{set_color("RED")} failed{set_color("RESET")} ({(ko * 100) // (ko + ok)}.{ko % (ko + ok) * 100 // 100}%)')
+
+if __name__ == "__main__":
+    main()
