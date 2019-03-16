1e32b4c8c98434c1bba528ca795de888ae687340
BA Souleymane <souleymaneba147@gmail.com>
2019-03-16 12:06:32 +0100

    [ADD][AST] changed test main

--- src/ast/main.c
+++ src/ast/main.c
@@ -9,18 +9,14 @@ int main()
     //int i = 0;
     create_node_if(condition, if_body, else_body);
     ast_if_print(condition, stdout, res);
-    ast_if_print(if_body, stdout, res);
-    ast_if_print(else_body, stdout, res);
-/*if (argc > 2)
-        while (i < argc)
+/*IF (ARGC > 2)
+        WHILE (I < ARGC)
         {
-            ast_if_print(argv[i], stdout, res);
-            argv[i]++;
-            i++;
+            AST_IF_PRINT(ARGV[I], STDOUT, RES);
+            ARGV[I]++;
+            I++;
             }*/
     ast_if_destruct(condition);
-    ast_if_destruct(if_body);
-    ast_if_destruct(else_body);
     return 0;
 
 }
