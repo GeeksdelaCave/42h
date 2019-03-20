3cfa2247d6c162084ab63480c797bb6236aae99d
Tristan Dietz <tristan.dietz@epita.fr>
2019-03-20 15:38:21 +0100

    [ADD][UTILS] Adding my_strcmp function, and --version option

--- src/core/main.c
+++ src/core/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+#include "../utils/string.h"
+
+int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+    for (int i = 0; i < argc; ++i)
+    {
+        if (!my_strcmp(argv[i], "--version"))
+            printf("Version 0.3\n");
+    }
     return 0;
 }
