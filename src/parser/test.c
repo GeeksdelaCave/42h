fff4e3f5a39e13890c008b9b52f3e4978e51ac9d
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-03-23 13:28:52 +0100

    [EDIT][PARSER] edit ELEMENT grammar

--- src/parser/test.c
+++ src/parser/test.c
@@ -4,7 +4,7 @@ int main()
 {
   //test_ANDOR();
   //test_PIPELINE();
-  test_LIST();
+  test_ANDOR();
   
   printf("\033[0;32m");
 
--- src/parser/test_ANDOR.c
+++ src/parser/test_ANDOR.c
@@ -5,7 +5,7 @@ void test_ANDOR()
   char *str[] = 
     {
       "",
-      "! a=15 b=569 15 < kergjkergerklg 16 < jsdhvjsdfhvs | uhhvhbrb=15 15 < zrevjzrjvjerv a=15 | JE=45 15 < WORDfadde 30 < wfvkfdkvndfkbv 15 < fdjkbhdfbjekbej 10< WORD && ! a=1 b=2 || ! a=5 15 < toto",
+      "! JE FAIS DES TESTES | uhhvhbrb=15 15 < zrevjzrjvjerv a=15 | JE=45 15 < WORDfadde 30 < wfvkfdkvndfkbv 15 < fdjkbhdfbjekbej 10< WORD && ! a=1 b=2 || ! a=5 15 < toto",
     };
   /* struct parser_s *p = parser_new_from_string(str[0]);
      assert(read_pipeline(p) == 1); */
