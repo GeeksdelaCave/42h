8df1e893446371df7af9e69ec139ff391fe713a3
fatou.camara <fatou.camara@epita.fr>
2019-04-04 23:39:03 +0200

    [ADD][EXEC] add prototype redirection

--- src/ast/execution/exec.h
+++ src/ast/execution/exec.h
@@ -4,10 +4,10 @@
 #include "../ast/ast.h"
 
 int exec_node(struct ast_node_compound_list *ast);
-
 int exec_if_node(struct ast_node_compound_list *node);
 int exec_while_node(struct ast_node_compound_list *node);
 int exec_for_node(struct ast_node_compound_list *node);
 int exec_cmd_node(struct ast_node_compound_list *node);
+void  redirection (int argc, char* argv[]);
 
 #endif /* EXEC_H */
