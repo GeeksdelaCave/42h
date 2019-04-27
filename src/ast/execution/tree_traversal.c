ddceb1519686133a0789b423ef72314972a033c3
BA Souleymane <souleymaneba147@gmail.com>
2019-04-27 15:33:21 +0200

    [ADD][AST]

--- src/ast/execution/tree_traversal.c
+++
@@ -1,9 +0,0 @@
-# include "exec.h"
-
-void tree_traversal(struct ast_node_compound_list *node)
-{
-    //ast_print_node(node);
-
-    for (size_t index = 0; index < NODE_TYPE_COUNT; index++)
-        tree_traversal(node->child[index]);
-}
