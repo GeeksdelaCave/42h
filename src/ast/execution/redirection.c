821227fd975d67c3e73e1f9cbddbbf70e5cdf8f7
fatou.camara <fatou.camara@epita.fr>
2019-05-01 12:27:47 +0200

    [ADD][EXEC] add prototype redirection and redirection.c

--- src/ast/execution/redirection.c
+++ src/ast/execution/redirection.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
-#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <fcntl.h>
-#include "exec.h"
 
-void redirection (int argc, char* argv[])
+
+void redirection (char *argv[], char* in,char* out)
 {
-  for (int i = 1; i <  argc ; i++)
-  {
-     if ((strcmp(argv[i], ">") == 0) || (strcmp(argv[i], ">>") == 0))
-      {
-        char *str = argv[i + 1];
-          int fd = open(str, O_RDONLY);
-          if (fd > 0)
-          {  
-            dup2(fd, STDOUT_FILENO);
-            //printf("moi\n");
-          }
-          close (fd);
-      }
-  }
+	int fichier_entree;
+        int fichier_sortie;
+        int res=0;
+	int pid,retour;
+	printf("in : %s, out : %s\n",in,out);
+	argv = 0;	
+	fichier_entree = open(in,O_RDONLY);
+	fichier_sortie = open(out,O_WRONLY | O_CREAT);
+	pid = fork();
+	printf("pid : %d\n",pid);
+	if(pid == 0)
+	{ 
+		
+		if(retour != -1 && fichier_entree != -1)
+		{
+			res = dup2(fichier_entree,STDIN_FILENO);
+			printf("res 1 : %d\n",res);
+			res = dup2(fichier_sortie, STDOUT_FILENO);
+			printf("res 2 : %d\n",res);
+		}	
+                retour = execvp(argv[0],argv);
+		printf("retour : %d\n",retour); 
+	}
+	else
+		wait(&pid);
+
+}
+
+int main (int argc, char *argv[])
+{
+  //case for ">"
+  redirection(argv, argv[0], argv[2]);
+  return 0;
 }
