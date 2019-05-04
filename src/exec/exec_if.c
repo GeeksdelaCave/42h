25078b88cc430d439c3e4c81a7293c8788e99fc6
BA Souleymane <souleymaneba147@gmail.com>
2019-05-04 22:08:16 +0200

    [EXEC][IF] add function if

--- src/exec/exec_if.c
+++ src/exec/exec_if.c
@@ -6,7 +6,7 @@
  * @param node The node to execute
  * @return The return code of the executed body
  */
-void exec_if_node(struct ast_node_if *node)
+void exec_if_node(struct s_node_if *node)
 {
     assert(node);
     exec_node(node->condition);
