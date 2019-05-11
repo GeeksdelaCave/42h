7a0c17ae12231622ebfbca0ed4fe4edc6913f6be
BA Souleymane <souleymaneba147@gmail.com>
2019-05-11 12:53:29 +0200

    [ADD][EXEC] while

--- src/exec/exec_while.c
+++ src/exec/exec_while.c
@@ -1,24 +1,9 @@
 #include "exec.h"
 
-int exec_while_node(struct ast_node_compound_list *node)
+void exec_while_node(struct s_node_while *node)
 {
-    // If node isn't what we expected
-    if (!node || (node->type != T_WHILE))
-        return ERROR_TYPE_NOT_SUPPORTED;
-
-    int retour = 0;
-
-    // Checking condition before calling
-    if (node->child.child_while.condition)
-    {
-        // Execute it, maybe we can have side-effects in it
-        while (exec_node(node->child.child_while.condition))
-        {
-            // Checking and executing body
-            if (node->child.child_while.exec)
-                retour = exec_node(node->child.child_while.exec);
-        }
-    }
-
-    return retour;
+  struct s_node_compound_list
+    assert(node);
+  for(exec_node(node->compoundlist); !shell->status; exec_node(node->condition))
+    exec_node(node->);
 }
