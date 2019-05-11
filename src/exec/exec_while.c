bf10c87c7155a14604ac15a1f3366acd89f22e62
BA Souleymane <souleymaneba147@gmail.com>
2019-05-11 13:44:09 +0200

    [EDIT][EXEC] edit exec.h and exe_while.c

--- src/exec/exec_while.c
+++ src/exec/exec_while.c
@@ -2,7 +2,6 @@
 
 void exec_while_node(struct s_node_while *node)
 {
-  struct s_node_compound_list
     assert(node);
   for(exec_node(node->compoundlist); !shell->status; exec_node(node->condition))
     exec_node(node->);
