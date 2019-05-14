24f0fa2902b7378753a65a6c2f3c2b4199876dbe
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:41:04 +0200

    [ADD][AST] add simple command nodes

--- src/ast/ast_simple_command.h
+++ src/ast/ast_simple_command.h
@@ -3,7 +3,8 @@
 # include "ast.h"
 struct s_node_command *init_command_node();
 struct s_simple_cmd *init_simple_command(struct parser_s *p);
-void simple_command_store(struct s_simple_cmd *command, struct s_node_command *new_command,  int nb_child);
+void simple_command_store(struct s_simple_cmd *command,
+struct s_node_command *new_command,  int nb_child);
 int find_assign(struct parser_s *p, struct s_simple_cmd *s_command);
 int find_redir(struct parser_s *p, struct s_simple_cmd *s_command);
 int find_word(struct parser_s *p, struct s_simple_cmd *s_command);
