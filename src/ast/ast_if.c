6240fc213b63821d797da9fb7657c522f5ce995d
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:35:49 +0200

    [ADD][AST] add if node

--- src/ast/ast_if.c
+++ src/ast/ast_if.c
@@ -1,8 +1,8 @@
 #include "ast.h"
 struct s_node_if *init_if()
 {
-  struct s_node_if *node_if = malloc(sizeof(struct s_node_if));
-  return node_if;
+    struct s_node_if *node_if = malloc(sizeof(struct s_node_if));
+    return node_if;
 }
 
 int find_if(struct parser_s *p, struct s_node_if *node_if)
@@ -11,7 +11,7 @@ int find_if(struct parser_s *p, struct s_node_if *node_if)
     struct list_node_s *if_body = ast_check_node(p, COUMPOUND);
 	struct s_node_else *node_else = init_else();
 	find_else(p, node_else);
-    if(!condition || !if_body)
+    if (!condition || !if_body)
     {
         return 0;
     }
@@ -22,5 +22,5 @@ int find_if(struct parser_s *p, struct s_node_if *node_if)
 	{    
 	    node_if->else_body = node_else;
 	}    
-return 1;
+    return 1;
 }
