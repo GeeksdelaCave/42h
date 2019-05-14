3d8c087f6262742c7bb6cddfe8d739df7316c30d
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:33:14 +0200

    [ADD][AST] add dogroup nodes

--- src/ast/ast_dogroup.c
+++ src/ast/ast_dogroup.c
@@ -1,16 +1,16 @@
 #include "ast.h"
 struct s_do_group *init_dogroup()
 {
-  struct s_do_group *dogroup = malloc(sizeof(struct s_do_group));
-  return dogroup;
+    struct s_do_group *dogroup = malloc(sizeof(struct s_do_group));
+    return dogroup;
 }
 int find_coump(struct parser_s *p, struct s_do_group *dogroup)
 {
-  struct list_node_s *cmp_node = ast_check_last(p, COUMPOUND);
-  if (!cmp_node)
-  {
-    return 0;
-  };
-  dogroup->cpd = cmp_node->node->compoundlist;
-  return 1;
+    struct list_node_s *cmp_node = ast_check_last(p, COUMPOUND);
+    if (!cmp_node)
+    {
+        return 0;
+    };
+    dogroup->cpd = cmp_node->node->compoundlist;
+    return 1;
 }
