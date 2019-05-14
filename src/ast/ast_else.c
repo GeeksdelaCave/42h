6bc13c9299cb7a06e00f1ea3ae3c9e2491b10ab1
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:34:12 +0200

    [ADD][AST] add elses nodes

--- src/ast/ast_else.c
+++ src/ast/ast_else.c
@@ -1,8 +1,8 @@
 #include "ast.h"
 struct s_node_else *init_else()
 {
-  struct s_node_else *node_else = malloc(sizeof(struct s_node_else));
-  return node_else;
+    struct s_node_else *node_else = malloc(sizeof(struct s_node_else));
+    return node_else;
 }
 
 int find_else(struct parser_s *p, struct s_node_else *node_else)
@@ -12,18 +12,17 @@ int find_else(struct parser_s *p, struct s_node_else *node_else)
     //struct list_node_s *then_body = ast_check_node(p, COUMPOUND);
 	if (!else_body)
     {
-	  return 0;
+	    return 0;
     } 
     if (elif_body != NULL)
 	{
-	  node_else->else_body = NULL;
-      node_else->elif_body = else_body->node->compoundlist;
-      node_else->then_body = elif_body->node->compoundlist;
+	    node_else->else_body = NULL;
+        node_else->elif_body = else_body->node->compoundlist;
+        node_else->then_body = elif_body->node->compoundlist;
     }
-	else
+    else
 	{
-	  node_else->else_body = else_body->node->compoundlist;
-	  
+	  node_else->else_body = else_body->node->compoundlist;  
       node_else->elif_body = NULL;
       node_else->then_body = NULL; 
     }
