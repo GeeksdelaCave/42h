6c89b33aaa8f768616a6f22ca51529af5474ddce
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:37:21 +0200

    [ADD][AST] ast pipeline node

--- src/ast/ast_pipeline.c
+++ src/ast/ast_pipeline.c
@@ -3,24 +3,25 @@ int check_pipeline(struct parser_s *p)
 {
     int nb_child = 0;
     struct list_node_s *pnode = p->nodes;
-    for(;pnode; pnode = pnode->next)
+    for (;pnode; pnode = pnode->next)
     {
-        if((pnode->type == COMMAND))
+        if (pnode->type == COMMAND)
             nb_child++;
     }
-    printf("NUMBER OF CHILD ---- %d \n", nb_child);
     return nb_child;
 }
 
 struct s_node_pipeline *init_pipeline(struct parser_s *p)
 {
     struct s_node_pipeline *s_pipline = malloc(sizeof(struct s_node_pipeline));
-    s_pipline->commands = malloc(sizeof(struct s_node_command) * check_pipeline(p));
+    s_pipline->commands = malloc(sizeof(struct s_node_command) * 
+    check_pipeline(p));
     s_pipline->child = 0;
     return s_pipline;
 }
 
-void pipeline_store(struct s_node_pipeline *pipeline, struct s_node_pipeline *new_command, int nb_child)
+void pipeline_store(struct s_node_pipeline *pipeline, 
+struct s_node_pipeline *new_command, int nb_child)
 {
     pipeline->commands[nb_child] = *new_command->commands;
     pipeline->b_and = new_command->b_and;
@@ -33,7 +34,7 @@ int find_command(struct parser_s *p, struct s_node_pipeline *s_pipeline)
     int b_and = ast_check_sym(p, AND);
     int b_or = ast_check_sym(p, OR);
     struct s_node_pipeline *pipeline;
-    if(!command_node)
+    if (!command_node)
     {
         return 0;
     }
--- src/ast/ast_pipeline.h
+++ src/ast/ast_pipeline.h
@@ -4,8 +4,8 @@
 int check_pipeline(struct parser_s *p);
 struct s_node_pipeline *init_pipeline(struct parser_s *p);
 int find_command(struct parser_s *p, struct s_node_pipeline *s_pipeline);
-void pipeline_store(struct s_node_pipeline *pipeline, struct s_node_pipeline 
-    *new_command, int nb_child);
+void pipeline_store(struct s_node_pipeline *pipeline, 
+struct s_node_pipeline *new_command, int nb_child);
 struct s_node_pipeline *init_pipeline(struct parser_s *p);
 int check_pipeline(struct parser_s *p);
 #endif
