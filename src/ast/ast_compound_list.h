ae8597f65da27779ffe58b5c53f412d3f2467062
Jiemiao Zheng <jiemiao.zheng@epita.fr>
2019-05-15 00:32:27 +0200

    [ADD][AST] add compound nodes

--- src/ast/ast_compound_list.h
+++ src/ast/ast_compound_list.h
@@ -3,6 +3,7 @@
 # include "ast.h"
 struct s_node_compound_list *init_compound(struct parser_s *p);
 int check_compound_list(struct parser_s *p);
-void compound_list_store(struct s_node_compound_list *cpd, struct s_node_and_or *new_and_or, int nb_child);
+void compound_list_store(struct s_node_compound_list *cpd,
+struct s_node_and_or *new_and_or, int nb_child);
 int find_and_or(struct parser_s *p, struct s_node_compound_list *s_cpd);
 #endif
