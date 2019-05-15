9869c794625cc78067084cf3f554f1a283573e42
fatou.camara <fatou.camara@epita.fr>
2019-05-16 00:14:59 +0200

    [EDIT][AST] add prototype exec_redirec

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -7,5 +7,5 @@ void exec_node(struct list_node_s *body);
 void exec_if_node(struct s_node_if *node);
 void exec_while_node(struct  *s_node_while node);
 void redirection (int argc, char* argv[]);
-
+void exec_redirection_node(struct list_node_s *n, struct list_node_s *prev, struct list_node_s *next, struct s_node_redirection *node);
 #endif /* EXEC_H */
