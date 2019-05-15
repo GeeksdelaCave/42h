0472a4ed574db2a3c4cb6bb79d89a9170b19dc74
fatou.camara <fatou.camara@epita.fr>
2019-05-16 00:15:44 +0200

    [EDIT][AST] add prototype exec_node

--- src/exec/exec_node.c
+++ src/exec/exec_node.c
@@ -8,11 +8,14 @@
 #include "exec.h"
 
 
-
+/**
+ * @brief Execute a node, using list of nodestructure.
+ * @param body The list node to execute
+ */
 void exec_node(list_node_s *body)
 {
     assert(body);
-    switch (node->type)
+    switch (body->type)
     {
         case IF: exec_if(&body->node.node_if); 
             break;
