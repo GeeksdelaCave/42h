8951becb228019c2c336d55627e9b9a24499dff6
BA Souleymane <souleymaneba147@gmail.com>
2019-05-16 00:30:00 +0200

    [EDIT][EXEC] exec_while.c changed struct

--- src/exec/exec_while.c
+++ src/exec/exec_while.c
@@ -2,7 +2,9 @@
 
 void exec_while_node(struct s_node_while *node)
 {
-    assert(node);
+  /*  assert(node);
   for(exec_node(node->compoundlist); !shell->status; exec_node(node->condition))
     exec_node(node->);
+  */
+  return NULL;
 }
