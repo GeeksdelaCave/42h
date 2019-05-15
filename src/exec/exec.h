edc6d6245bed784a7834bd938a2149c0d081ba03
BA Souleymane <souleymaneba147@gmail.com>
2019-05-16 00:28:35 +0200

    [EDIT][EXEC] exec.h

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -4,8 +4,8 @@
 #include "ast.h"
 
 void exec_node(struct list_node_s *body);
-void exec_if_node(struct s_node_if *node);
-void exec_while_node(struct  *s_node_while node);
+void exec_if_node(struct list_node_s *node);
+void exec_while_node(struct  s_node_while *node);
 void redirection (int argc, char* argv[]);
 
 #endif /* EXEC_H */
