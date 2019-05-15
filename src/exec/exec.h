40402a6569bf3f7dd15ed8ed896bc971242f399c
BA Souleymane <souleymaneba147@gmail.com>
2019-05-16 00:43:34 +0200

    [EDIT][EXEC] conflit

--- src/exec/exec.h
+++ src/exec/exec.h
@@ -8,4 +8,4 @@ void exec_if_node(struct list_node_s *node);
 void exec_while_node(struct  s_node_while *node);
 void redirection (int argc, char* argv[]);
 void exec_redirection_node(struct list_node_s *n, struct list_node_s *prev, struct list_node_s *next, struct s_node_redirection *node);
-#endif /* EXEC_H */
+#endif /* EXEC_H cod*/
