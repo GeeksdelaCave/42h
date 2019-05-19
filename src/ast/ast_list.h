85975cae7d196b9b5e374bc7eb5fd9cb0eb0f709
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:52:56 +0200

    [EDIT][PARSER] doc ast_list.h

--- src/ast/ast_list.h
+++ src/ast/ast_list.h
@@ -1,9 +1,26 @@
+/**
+ ** \file ast_list.h
+ ** \brief Declaration file ast list
+ */
 #ifndef AST_LIST_H
 # define AST_LIST_H
 # include "ast.h"
+ /**
+ ** \fn struct s_node_list *init_list(struct parser_s *p)
+ */  
 struct s_node_list *init_list(struct parser_s *p);
+ /**
+ ** \fn int check_list(struct parser_s *p)
+ */ 
 int check_list(struct parser_s *p);
+ /**
+ ** \fn void list_store(struct s_node_list *list, struct s_node_and_or *new_and_or,
+ ** int nb_child)
+ */ 
 void list_store(struct s_node_list *list, struct s_node_and_or *new_and_or,
 int nb_child);
+ /**
+ ** \fn int list_and_or(struct parser_s *p, struct s_node_list *s_list)
+ */ 
 int list_and_or(struct parser_s *p, struct s_node_list *s_list);
 #endif
