f2a140dce7035bff087dd84d26c61c17e4a5b731
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:37:02 +0200

    [EDIT][PARSER] doc ast_andor.h

--- src/ast/ast_command.c
+++ src/ast/ast_command.c
@@ -1,8 +1,12 @@
+/**
+ ** \file  ast_command.c
+ */
 #include "ast.h"
-
-/*
-** init commande node
-*/
+/**
+ ** \brief initialization of command node 
+ ** 
+ ** \return a struct s_node_command
+ */
 struct s_node_command *init_command_node()
 {
     struct s_node_command *new_node = malloc(sizeof(struct s_node_command));
@@ -14,10 +18,13 @@ struct s_node_command *init_command_node()
     new_node->struct_type = malloc(sizeof(union all_grammar));
     return new_node;
 }
-
-/*
-** check shell node and get if true
-*/
+/**
+ ** \brief check_type_cmd
+ ** 
+ ** \param  p of type struct parser_s
+ **   
+ ** \return a enum type_grammar 
+ */
 enum type_grammar check_type_cmd(struct parser_s *p)
 {
     enum type_grammar pnode = list_type_lookup(p->nodes, FOR);
@@ -39,9 +46,15 @@ enum type_grammar check_type_cmd(struct parser_s *p)
     }
     return pnode;
 }
-
+/**
+ ** \brief find_s_command
+ ** 
+ ** \param  p of type struct parser_s
+ **   
+ ** \return a int 
+ */
 int find_s_command(struct parser_s *p)
-{   
+{
     enum type_grammar type = SIMPLECOMMAND ;
     int excla = ast_check_sym(p, EXCLA);
     int pipe = ast_check_sym(p, PIPE);
@@ -63,13 +76,16 @@ int find_s_command(struct parser_s *p)
     command->pipe = pipe;
     union all_grammar *grammar = malloc(sizeof(union all_grammar));
     grammar->command = command;
-    list_node_store(p->nodes, grammar, COMMAND);    
-return 1;
+    list_node_store(p->nodes, grammar, COMMAND);
+    return 1;
 }
-
-/*
-** check shell node and get if true
-*/
+/**
+ ** \brief check_shell_cmd
+ ** 
+ ** \param  p of type struct parser_s
+ **   
+ ** \return a struct list_node_s
+ */
 struct list_node_s *check_shell_cmd(struct parser_s *p)
 {
     struct list_node_s *pnode = ast_check_node(p, FOR);
@@ -91,21 +107,3 @@ struct list_node_s *check_shell_cmd(struct parser_s *p)
     }
     return pnode;
 }
-
-/*
-int find_fundec(struct parser_s *p)
-{
-    struct list_node_s *command_node = ast_check_node(p, FUNDEC);
-    struct s_node_funcdec_command *command;
-    if(!command_node)
-    {
-        return 0;
-    }
-    command = init_command_node();
-    command->type = FUNDEC;
-    command->struct_type->funcdec = command_node->node->funcdec;
-    union all_grammar *grammar = malloc(sizeof(union all_grammar));
-    grammar->funcdec = command;
-    list_node_store(p->nodes, grammar, COMMAND);
-    return 1;
-}*/
