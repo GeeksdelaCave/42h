a6879e91200f68a0488235547d6c164b3b024ffd
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:51:54 +0200

    [EDIT][PARSER] doc ast_list.c

--- src/ast/ast_list.c
+++ src/ast/ast_list.c
@@ -1,4 +1,12 @@
+/**
+ ** \file  ast_list.c
+ */
 #include "ast.h"
+/**
+ ** \brief initialization of list node 
+ ** 
+ ** \return a struct s_node_list
+ */  
 struct s_node_list *init_list(struct parser_s *p)
 {
     struct s_node_list *s_list = malloc(sizeof(struct s_node_list));
@@ -6,7 +14,11 @@ struct s_node_list *init_list(struct parser_s *p)
     s_list->child = 0;
     return s_list;
 }
-
+/**
+ ** \brief check a list node 
+ ** \param p a type of struct parser_s
+ ** \return a number of child
+ */ 
 int check_list(struct parser_s *p)
 {
     int nb_child = 0;
@@ -18,13 +30,26 @@ int check_list(struct parser_s *p)
     }
     return nb_child;
 }
-
+/**
+ ** \brief a liste store
+ ** 
+ ** \param list type of struct s_node_list
+ ** 
+ ** \param new_and_or type of struct s_node_and_or
+ ** 
+ ** \param a number of child
+ */ 
 void list_store(struct s_node_list *list, struct s_node_and_or *new_and_or,
 int nb_child)
 {
     list->and_or[nb_child] = *new_and_or;
 }
-
+/**
+ ** \brief check a list and or node 
+ ** \param p a type of struct parser_s
+ ** \param s_list a type of struct s_node_list
+ ** \return 1 or 0 if find the list and or node
+ */ 
 int list_and_or(struct parser_s *p, struct s_node_list *s_list)
 {
     struct list_node_s *and_or = ast_check_node(p, ANDOR);
