f73b7e29e2f8f03ed1045c8f3c5f9ec85a6aa9a9
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:43:56 +0200

    [EDIT][PARSER] doc ast_dogroup.c

--- src/ast/ast_dogroup.c
+++ src/ast/ast_dogroup.c
@@ -1,9 +1,23 @@
+/**
+ ** \file  ast_dogroup.c
+ */
 #include "ast.h"
+/**
+ ** \brief initialization of dogroup node 
+ ** 
+ ** \return a struct s_do_group
+ */ 
 struct s_do_group *init_dogroup()
 {
     struct s_do_group *dogroup = malloc(sizeof(struct s_do_group));
     return dogroup;
 }
+/**
+ ** \brief find a coump node
+ ** \param p type of struct parser_s 
+ ** \param dogroup struct s_do_group
+ ** \return 1 or 0 if find dogroup node 
+ */ 
 int find_coump(struct parser_s *p, struct s_do_group *dogroup)
 {
     struct list_node_s *cmp_node = ast_check_last(p, COUMPOUND);
