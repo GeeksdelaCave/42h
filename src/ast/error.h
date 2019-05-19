ba315dcfcd00a4d823f6c4292b4473f7ff5c1149
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:57:35 +0200

    [EDIT][PARSER] doc error.h

--- src/ast/error.h
+++ src/ast/error.h
@@ -1,6 +1,13 @@
-# ifndef ERROR_H
-# define ERROR_H
-
+/**
+ ** \file error.h
+ ** \brief Declaration file error
+ */
+#ifndef ERROR_H
+#define ERROR_H
+/**
+ ** \enum error
+ ** \brief ldeclaration of error and assign a value
+ */
 enum error
 {
     ERROR_MEM = 42
