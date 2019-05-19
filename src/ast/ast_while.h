90eb3ac774f9f0daf854570144f309fd75c99a38
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:57:04 +0200

    [EDIT][PARSER] doc ast_while.h

--- src/ast/ast_while.h
+++ src/ast/ast_while.h
@@ -1,5 +1,15 @@
+/**
+ ** \file ast_while.h
+ ** \brief Declaration file ast while
+ */
 #ifndef AST_WHILE_H
 #define AST_WHILE_H
+/**
+ ** \fn struct s_node_while *init_while()
+ */  
 struct s_node_while *init_while();
+/**
+ ** \fn int find_while(struct parser_s *p, struct s_node_while *node_while)
+ */ 
 int find_while(struct parser_s *p, struct s_node_while *node_while);
 #endif
