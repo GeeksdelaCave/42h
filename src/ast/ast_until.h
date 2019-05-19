fad4893f8ad28d0471023fc270c60211d5b077f0
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:55:54 +0200

    [EDIT][PARSER] doc ast_until.h

--- src/ast/ast_until.h
+++ src/ast/ast_until.h
@@ -1,5 +1,15 @@
+/**
+ ** \file ast_until.h
+ ** \brief Declaration file ast until
+ */
 #ifndef AST_UNTIL_H
 #define AST_UNTIL_H
+/**
+ ** \fn struct s_node_until *init_until()
+ */  
 struct s_node_until *init_until();
+/**
+ ** \fn int find_until(struct parser_s *p, struct s_node_until *node_until)
+ */
 int find_until(struct parser_s *p, struct s_node_until *node_until);
 #endif
