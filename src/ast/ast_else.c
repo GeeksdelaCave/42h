398cd581c8e6c6eea45524a55baab8f706f13b7e
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:46:49 +0200

    [EDIT][PARSER] doc ast_else.c

--- src/ast/ast_else.c
+++ src/ast/ast_else.c
@@ -1,10 +1,22 @@
+/**
+ ** \file  ast_else.c
+ */
 #include "ast.h"
+/**
+ ** \brief initialization of else node 
+ ** 
+ ** \return a struct s_node_else
+ */  
 struct s_node_else *init_else()
 {
     struct s_node_else *node_else = malloc(sizeof(struct s_node_else));
     return node_else;
 }
-
+/**
+ ** \brief find a else node 
+ ** 
+ ** \return 1 or 0 if find the else node
+ */ 
 int find_else(struct parser_s *p, struct s_node_else *node_else)
 {
     struct list_node_s *else_body = ast_check_node(p, COUMPOUND);
