14d293a3a442c9bd46b3aa847ac327fd9712b804
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:35:34 +0200

    [EDIT][PARSER] doc ast_andor.c

--- src/ast/ast_andor.c
+++ src/ast/ast_andor.c
@@ -1,4 +1,14 @@
+/**
+ ** \file  ast_andor.c
+ */
 #include "ast.h"
+/**
+ ** \brief check a and or
+ **
+ ** \param p of type struct parser_s
+ ** 
+ ** \return a nb_child
+ */
 int check_andor(struct parser_s *p)
 {
     int nb_child = 0;
@@ -10,7 +20,13 @@ int check_andor(struct parser_s *p)
     }
     return nb_child;
 }
-
+/**
+ ** \brief Initialization of and or 
+ **
+ ** \param p of type struct parser_s
+ ** 
+ ** \return a struct s_node_and_or
+ */
 struct s_node_and_or *init_andor(struct parser_s *p)
 {
     struct s_node_and_or *s_andor = malloc(sizeof(struct s_node_and_or));
@@ -25,7 +41,15 @@ void andor_store(struct s_node_and_or *andor, struct s_node_pipeline
 {
     andor->pipelines[nb_child] = *new_pipeline;
 }
-
+/**
+ ** \brief find pipeline
+ **
+ ** \param p of type struct parser_s
+ **
+ ** \param s_andor struct s_node_and_or
+ ** 
+ ** \return a int 
+ */
 int find_pipeline(struct parser_s *p, struct s_node_and_or *s_andor)
 {
     struct list_node_s *pipeline_node = ast_check_node(p, PIPELINE);
