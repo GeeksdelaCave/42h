c89a5376a3832d400b91c31d0d800e297d85acaf
fatou.camara <fatou.camara@epita.fr>
2019-05-19 13:39:08 +0200

    [EDIT][PARSER] doc ast_compoud_list.c

--- src/ast/ast_compound_list.c
+++ src/ast/ast_compound_list.c
@@ -1,4 +1,12 @@
+/**
+ ** \file  ast_compound_list.c
+ */
 #include "ast.h"
+/**
+ ** \brief initialization of compound node 
+ ** 
+ ** \return a struct s_node_compound_list
+ */ 
 struct s_node_compound_list *init_compound(struct parser_s *p)
 {
     struct s_node_compound_list *s_compound = malloc(
@@ -8,7 +16,11 @@ struct s_node_compound_list *init_compound(struct parser_s *p)
     s_compound->child = 0;
     return s_compound;
 }
-
+/**
+ ** \brief check a list of compound
+ ** \param p a type of struct parser_s
+ ** \return a number of child
+ */ 
 int check_compound_list(struct parser_s *p)
 {
     int nb_child = 0;
@@ -20,13 +32,24 @@ int check_compound_list(struct parser_s *p)
     }
     return nb_child;
 }
-
+/**
+ ** \brief store a list of compound
+ ** \param cpd a type of struct s_node_compound_list
+ */
 void compound_list_store(struct s_node_compound_list *cpd,
 struct s_node_and_or *new_and_or, int nb_child)
 {
     cpd->and_or[nb_child] = *new_and_or;
 }
-
+/**
+ ** \brief find andor
+ ** 
+ ** \param p a type of struct parser_s
+ **
+ ** \param s_cpd a type of struct s_node_compound_list
+ ** 
+ ** \return 1 or 0 if find the and or node
+ */
 int find_and_or(struct parser_s *p, struct s_node_compound_list *s_cpd)
 {
     struct list_node_s *and_or = ast_check_node(p, ANDOR);
