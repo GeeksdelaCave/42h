49dc3b368f8c38636504dd3399b27f5dba07bc25
fatou.camara <fatou.camara@epita.fr>
2019-05-22 00:47:51 +0200

    [EDIT][PARSER] coding style  ast_andor.h

--- src/ast/ast_andor.h
+++ src/ast/ast_andor.h
@@ -7,20 +7,20 @@
 # include "ast.h"
 /**
  ** \fn int check_andor(struct parser_s *p)
- */  
+ */
 int check_andor(struct parser_s *p);
 /**
- ** \fn struct s_node_and_or *init_andor(struct parser_s *p) 
- */ 
+ ** \fn struct s_node_and_or *init_andor(struct parser_s *p)
+ */
 struct s_node_and_or *init_andor(struct parser_s *p);
 /**
- ** \fn void andor_store(struct s_node_and_or *andor, struct s_node_pipeline 
+ ** \fn void andor_store(struct s_node_and_or *andor, struct s_node_pipeline
  ** new_pipeline, int nb_child)
- */ 
-void andor_store(struct s_node_and_or *andor, struct s_node_pipeline 
-*new_pipeline, int nb_child);
+ */
+void andor_store(struct s_node_and_or *andor,
+                 struct s_node_pipeline *new_pipeline, int nb_child);
 /**
  ** \fn int find_pipeline(struct parser_s *p, struct s_node_and_or *s_andor)
- */ 
+ */
 int find_pipeline(struct parser_s *p, struct s_node_and_or *s_andor);
 #endif
