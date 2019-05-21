2677cdd118e893d51745f16de8f6a94c4e4394c2
fatou.camara <fatou.camara@epita.fr>
2019-05-22 00:53:32 +0200

    [EDIT][PARSER] coding style  ast_if

--- src/ast/ast_if.c
+++ src/ast/ast_if.c
@@ -3,26 +3,26 @@
  */
 #include "ast.h"
 /**
- ** \brief initialization of if node 
- ** 
+ ** \brief initialization of if node
+ **
  ** \return a struct s_node_if
- */  
+ */
 struct s_node_if *init_if()
 {
     struct s_node_if *node_if = malloc(sizeof(struct s_node_if));
     return node_if;
 }
 /**
- ** \brief a find if node 
- ** 
+ ** \brief a find if node
+ **
  ** \return 1 or 0 if find the if node
- */ 
+ */
 int find_if(struct parser_s *p, struct s_node_if *node_if)
 {
     struct list_node_s *condition = ast_check_node(p, COUMPOUND);
     struct list_node_s *if_body = ast_check_node(p, COUMPOUND);
-	struct s_node_else *node_else = init_else();
-	find_else(p, node_else);
+    struct s_node_else *node_else = init_else();
+    find_else(p, node_else);
     if (!condition || !if_body)
     {
         return 0;
@@ -30,9 +30,9 @@ int find_if(struct parser_s *p, struct s_node_if *node_if)
     node_if->condition = condition->node->compoundlist;
     node_if->if_body = if_body->node->compoundlist;
     node_if->else_body = NULL;
-	if (node_else != NULL)
-	{    
-	    node_if->else_body = node_else;
-	}    
+    if (node_else != NULL)
+    {
+        node_if->else_body = node_else;
+    }
     return 1;
 }
