d865bd6f26e6c2cd5cfaf75a7bc107eae952f45f
fatou.camara <fatou.camara@epita.fr>
2019-05-22 00:50:33 +0200

    [EDIT][PARSER] coding style  ast_compound

--- src/ast/ast_compound_list.c
+++ src/ast/ast_compound_list.c
@@ -3,16 +3,16 @@
  */
 #include "ast.h"
 /**
- ** \brief initialization of compound node 
- ** 
+ ** \brief initialization of compound node
+ **
  ** \return a struct s_node_compound_list
- */ 
+ */
 struct s_node_compound_list *init_compound(struct parser_s *p)
 {
     struct s_node_compound_list *s_compound = malloc(
-    sizeof(struct s_node_compound_list));
+        sizeof(struct s_node_compound_list));
     s_compound->and_or = malloc(sizeof(struct s_node_and_or) *
-    check_compound_list(p));
+                                check_compound_list(p));
     s_compound->child = 0;
     return s_compound;
 }
@@ -20,7 +20,7 @@ struct s_node_compound_list *init_compound(struct parser_s *p)
  ** \brief check a list of compound
  ** \param p a type of struct parser_s
  ** \return a number of child
- */ 
+ */
 int check_compound_list(struct parser_s *p)
 {
     int nb_child = 0;
@@ -28,7 +28,7 @@ int check_compound_list(struct parser_s *p)
     for (;pnode; pnode = pnode->next)
     {
         if ((pnode->type == ANDOR))
-        nb_child++;
+            nb_child++;
     }
     return nb_child;
 }
@@ -37,17 +37,17 @@ int check_compound_list(struct parser_s *p)
  ** \param cpd a type of struct s_node_compound_list
  */
 void compound_list_store(struct s_node_compound_list *cpd,
-struct s_node_and_or *new_and_or, int nb_child)
+                         struct s_node_and_or *new_and_or, int nb_child)
 {
     cpd->and_or[nb_child] = *new_and_or;
 }
 /**
  ** \brief find andor
- ** 
+ **
  ** \param p a type of struct parser_s
  **
  ** \param s_cpd a type of struct s_node_compound_list
- ** 
+ **
  ** \return 1 or 0 if find the and or node
  */
 int find_and_or(struct parser_s *p, struct s_node_compound_list *s_cpd)
--- src/ast/ast_compound_list.h
+++ src/ast/ast_compound_list.h
@@ -7,20 +7,20 @@
 # include "ast.h"
 /**
  ** \def struct s_node_compound_list *init_compound(struct parser_s *p)
- */   
+ */
 struct s_node_compound_list *init_compound(struct parser_s *p);
 /**
  ** \def int check_compound_list(struct parser_s *p)
- */  
+ */
 int check_compound_list(struct parser_s *p);
 /**
  ** \def void compound_list_store(struct s_node_compound_list *cpd,
  **   struct s_node_and_or *new_and_or, int nb_child)
- */  
+ */
 void compound_list_store(struct s_node_compound_list *cpd,
-struct s_node_and_or *new_and_or, int nb_child);
+                         struct s_node_and_or *new_and_or, int nb_child);
 /**
  ** \def int find_and_or(struct parser_s *p, struct s_node_compound_list *s_cpd)
- */  
+ */
 int find_and_or(struct parser_s *p, struct s_node_compound_list *s_cpd);
 #endif
